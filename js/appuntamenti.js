3d7eaa23a59fc4fb9ffd0c4d4327cd0bb5738c16
archistico <info@archistico.com>
2018-10-02 10:47:52 +0200

    scrolldown after reload

--- js/appuntamenti.js
+++ js/appuntamenti.js
@@ -13,46 +13,56 @@ $('#rimuoviModal').on('show.bs.modal', function (event) {
     <input type="hidden" name="data" value="" id="data">
     <input type="hidden" name="ora" value="" id="ora">
     */
-    
-    document.getElementById("tipologia").value=0;
-    document.getElementById("data").value=data;
-    document.getElementById("ora").value=ora;
+
+    document.getElementById("tipologia").value = 0;
+    document.getElementById("data").value = data;
+    document.getElementById("ora").value = ora;
 });
 
 function btn_annullo() {
-    document.getElementById("tipologia").value='annullato';
+    document.getElementById("tipologia").value = 'annullato';
     document.getElementById("form_rimuovi").submit();
 }
 
 function btn_fatto() {
-    document.getElementById("tipologia").value='fatto';
+    document.getElementById("tipologia").value = 'fatto';
     document.getElementById("form_rimuovi").submit();
 }
 
 function btn_nonpresentato() {
-    document.getElementById("tipologia").value='assente';
+    document.getElementById("tipologia").value = 'assente';
     document.getElementById("form_rimuovi").submit();
 }
 
 $('#aggiungiModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); 
+    var button = $(event.relatedTarget);
 
-    var data = button.data('adddata'); 
-    var ora = button.data('addora'); 
+    var data = button.data('adddata');
+    var ora = button.data('addora');
     var giorno = button.data('addgiorno');
-    var ambulatorio = button.data('addambulatorio'); 
+    var ambulatorio = button.data('addambulatorio');
 
     var modal = $(this)
-    
+
     modal.find('#modal-add-data').text(giorno + " " + data);
     modal.find('#modal-add-ora').text('Ore: ' + ora);
     modal.find('#modal-add-ambulatorio').text('Ambulatorio: ' + ambulatorio);
 
-    document.getElementById("add-ora").value=ora;
-    document.getElementById("add-data").value=data;
+    document.getElementById("add-ora").value = ora;
+    document.getElementById("add-data").value = data;
 });
 
 function btn_aggiungi() {
     // controllare che il nome e cognome siano inseriti
     document.getElementById("form_aggiungi").submit();
 }
+
+$(window).scroll(function () {
+    sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function () {
+    if (sessionStorage.scrollTop != "undefined") {
+        $(window).scrollTop(sessionStorage.scrollTop);
+    }
+});
