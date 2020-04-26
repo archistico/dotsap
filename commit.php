9cfbbb4368bfdf8e49628f28870fba0afbe54ce2
unknown <info@archistico.com>
2020-04-26 11:05:01 +0200

    versione automatica

---
+++ commit.php
@@ -0,0 +1,12 @@
+<?php
+
+$nomeCommit = addslashes((string)$argv[1]);
+
+echo "|-------------------------------|\n";
+echo "|--------- NUOVO COMMIT --------|\n";
+echo "|-------------------------------|\n";
+echo "Nome: $nomeCommit\n";
+exec("git add .");
+exec('git commit -m"'.$nomeCommit.'"')."\n";
+//echo exec("git push origin master")."\n";
+//exec("git describe --all --long | cut -d '/' -f 2 > version.txt");
