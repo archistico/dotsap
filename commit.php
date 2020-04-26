e2bd85229862afc1ca2c4793af799307bb97721d
unknown <info@archistico.com>
2020-04-26 11:12:57 +0200

    php autoversione

--- commit.php
+++ commit.php
@@ -10,17 +10,20 @@ if (count($argv)==1) {
 
 $nomeCommit = addslashes((string)$argv[1]);
 if (count($argv)==3) {
-    $tag = $argv[2];
+    $tag = (string)$argv[2];
 }
 
 echo "|-------------------------------|\n";
 echo "|--------- NUOVO COMMIT --------|\n";
 echo "|-------------------------------|\n";
 echo "Nome: $nomeCommit\n";
-if (!empty($tag)) {
-    echo "Nome: $nomeCommit\n";
-}
 exec("git add .");
-exec('git commit -m"'.$nomeCommit.'"')."\n";
-//echo exec("git push origin master")."\n";
-//exec("git describe --all --long | cut -d '/' -f 2 > version.txt");
+
+if (!empty($tag)) {
+    echo "Tag: $tag\n";
+    exec("git tag -a \"v$tag\" -m \"version v$tag\"");
+}
+
+exec('git commit -m"'.$nomeCommit.'"');
+exec("git push origin master");
+exec("git describe --all --long | cut -d '/' -f 2 > version.txt");
