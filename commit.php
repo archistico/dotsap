dedb8c0678f7b22b7038667b3f718031420111ad
unknown <info@archistico.com>
2020-04-26 11:09:45 +0200

    php bozza versione automatica

--- commit.php
+++ commit.php
@@ -1,12 +1,25 @@
 <?php
 
+$nomeCommit = "";
+$tag = "";
+
+if (count($argv)==1) {
+    echo "Specificare il nome del commit tra apici doppi \"...\" ";
+    die();
+}
+
 $nomeCommit = addslashes((string)$argv[1]);
-echo count($argv);
+if (count($argv)==3) {
+    $tag = $argv[2];
+}
 
 echo "|-------------------------------|\n";
 echo "|--------- NUOVO COMMIT --------|\n";
 echo "|-------------------------------|\n";
 echo "Nome: $nomeCommit\n";
+if (!empty($tag)) {
+    echo "Nome: $nomeCommit\n";
+}
 exec("git add .");
 exec('git commit -m"'.$nomeCommit.'"')."\n";
 //echo exec("git push origin master")."\n";
